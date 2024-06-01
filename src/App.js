60dbe51f4fe4899353932a882e6747f2387ed66a
jlanp3 <jlanp3@gmail.com>
2024-06-01 19:32:37 -0400

    FCC All test passed, just need add more styles

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useRef, useEffect, useState } from "react";
 import "./App.css";
 import Control from "./Components/Control";
 import Timer from "./Components/Timer";
@@ -6,17 +6,57 @@ import Timer from "./Components/Timer";
 function App() {
   const [seLeng, setSeLeng] = useState(25);
   const [brLeng, setBrLeng] = useState(5);
+  const [min, setMin] = useState(25);
+  const [sec, setSec] = useState(0);
+  const [pause, setPause] = useState(false);
+  const [active, setActive] = useState(true);
+  const [mode, setMode] = useState("Session");
+
+  const interval = useRef(null);
 
   let nameControls = ["Break", "Session"];
+
+  useEffect(() => {
+    if (pause) {
+      interval.current = setInterval(() => {
+        if (min === 0 && sec === 0) {
+          if (active) {
+            setMin(brLeng);
+            setMode("Break");
+            setActive(!active);
+          } else {
+            setMin(seLeng);
+            setMode("Session");
+            setActive(!active);
+          }
+          document.querySelector("#beep").play();
+
+          clearInterval(interval.current);
+        }
+        if (min > 0 && sec === 0) {
+          setSec(59);
+          setMin((prev) => {
+            return prev - 1;
+          });
+        }
+        if (sec > 0) {
+          setSec((prev) => {
+            return prev - 1;
+          });
+        }
+      }, 1000);
+    }
+    return () => clearInterval(interval.current);
+  }, [pause, sec, min, brLeng, seLeng, mode, active]);
 
   const handleClick = (e) => {
     if (e.target.matches("button")) {
       if (e.target.matches("#start_stop")) {
-        console.log("start");
+        setPause(!pause);
       }
 
       if (e.target.matches("#reset")) {
-        return reset();
+        reset();
       }
 
       timerLength(e.target);
@@ -24,17 +64,24 @@ function App() {
   };
 
   const reset = () => {
+    clearInterval(interval.current);
+    interval.current = null;
+    setMode("Session");
+    setPause(false);
+    setActive(true);
+    setMin(25);
+    setSec(0);
     setSeLeng(25);
     setBrLeng(5);
+    document.querySelector("#beep").pause();
+    document.querySelector("#beep").currentTime = 0;
   };
 
-  const timer = () => {
-    setTimeout(() => {}, 1000);
-  };
+  console.log(mode);
 
   const timerLength = (data) => {
     if (data.matches("#break-decrement")) {
-      if (brLeng > 0) {
+      if (brLeng > 1) {
         return setBrLeng(brLeng - 1);
       }
     }
@@ -44,13 +91,17 @@ function App() {
       }
     }
     if (data.matches("#session-decrement")) {
-      if (seLeng > 0) {
-        return setSeLeng(seLeng - 1);
+      if (seLeng > 1) {
+        let newSession = seLeng - 1;
+        setSeLeng(newSession);
+        setMin(newSession);
       }
     }
     if (data.matches("#session-increment")) {
       if (seLeng < 60) {
-        return setSeLeng(seLeng + 1);
+        let newSession = seLeng + 1;
+        setSeLeng(newSession);
+        setMin(newSession);
       }
     }
   };
@@ -70,7 +121,7 @@ function App() {
             />
           );
         })}
-        <Timer brLeng={brLeng} seLeng={seLeng} handleClick={handleClick} />
+        <Timer min={min} sec={sec} mode={mode} handleClick={handleClick} />
       </div>
     </div>
   );
--- src/Components/Timer.js
+++ src/Components/Timer.js
@@ -1,10 +1,20 @@
-const Timer = ({ handleClick, seLeng, brLeng }) => {
+const Timer = ({ handleClick, min, sec, mode }) => {
   return (
-    <div className="timer" id="timer-label" onClick={handleClick}>
-      <h2>Session</h2>
-      <p id="time-left">{seLeng}:00</p>
+    <div className="timer" onClick={handleClick}>
+      <div id="timer-label">
+        <h2>{mode}</h2>
+      </div>
+      <p>Time Left</p>
+      <p id="time-left">
+        {min < 10 ? "0" + min : min}:{sec < 10 ? "0" + sec : sec}
+      </p>
       <button id="start_stop">Start/Stop</button>
       <button id="reset">Reset</button>
+      <audio
+        id="beep"
+        src="https://www.pacdv.com/sounds/interface_sound_effects/sound10.mp3"
+        type="audio/mp3"
+      ></audio>
     </div>
   );
 };
